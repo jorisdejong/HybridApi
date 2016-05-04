5ff609752faeadfd93683d293d70168b98ff072c
Joris de Jong <joris@hybridvisuals.nl>
2016-05-05 00:58:09 +0200

    First commit

---
+++ Source/HybridApi.h
@@ -0,0 +1,19 @@
+/*
+  ==============================================================================
+
+    HybridApi.h
+    Created: 4 May 2016 7:33:10pm
+    Author:  Joris de Jong
+
+  ==============================================================================
+*/
+
+#ifndef HYBRIDAPI_H_INCLUDED
+#define HYBRIDAPI_H_INCLUDED
+
+
+#include "JuceBased/ArenaHelpers/File/FileHelper.h"
+#include "JuceBased/ArenaHelpers/Xml/ResXmlParser.h"
+
+
+#endif  // HYBRIDAPI_H_INCLUDED
---
+++ Source/JuceBased/ArenaHelpers/Xml/ResXmlParser.h
@@ -0,0 +1,57 @@
+/*
+  ==============================================================================
+
+    ResXmlParser.h
+    Created: 9 Sep 2015 8:45:41pm
+    Author:  Joris de Jong
+
+  ==============================================================================
+*/
+
+#ifndef RESXMLPARSER_H_INCLUDED
+#define RESXMLPARSER_H_INCLUDED
+
+#include "JuceHeader.h"
+//#include "Slice.h"
+
+//helper class to parse xml data in the various resolume files
+
+class ResXmlParser
+{
+public:
+	ResXmlParser();
+	~ResXmlParser();
+	
+	//static bool parseAssXml ( File f, OwnedArray<Slice>& slices, Point<int>& resolution );
+	
+	//TODO make this take a File, not a XmlElement
+	static String getAdvancedPresetNameFromRes5Xml( XmlElement& xmlTreeToParse );
+    
+    static StringArray getScreenNames ( File assFile );
+	
+	
+	
+private:
+    /**
+    This function can take both the advanced.xml as well as a saved preset file, and extract the screensetup element
+     */
+    static XmlElement* getMainPresetElement ( File assFile );
+    
+    /*
+    
+	static bool parseRes4Xml( XmlElement& xmlTreeToParse, OwnedArray<Slice>& slices, Point<int>& resolution );
+	static bool parseRes5Xml( XmlElement& xmlTreeToParse, OwnedArray<Slice>& slices, Point<int>& resolution );
+	static bool parseRes5PrefXml ( XmlElement& xmlTreeToParse, OwnedArray<Slice>& slices, Point<int>& resolution );
+	static bool parseRes4ConfigXml ( XmlElement& xmlTreeToParse, OwnedArray<Slice>& slices, Point<int>& resolution );
+     */
+	
+	
+	
+	static void addPointToSlice ( XmlElement* sliceElement, Array<Point<float>>& pointType, Point<int> resolution);
+	
+	
+};
+
+
+
+#endif  // RESXMLPARSER_H_INCLUDED
